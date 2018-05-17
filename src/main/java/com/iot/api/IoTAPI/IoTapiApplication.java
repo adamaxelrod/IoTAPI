5c9153809f215c8e1e85a5a02349535be26318c5
Adam Axelrod <adam.axelrod@gmail.com>
2018-05-16 21:05:27 -0400

    initial commit of boilerplate spring project

---
+++ src/main/java/com/iot/api/IoTAPI/IoTapiApplication.java
@@ -0,0 +1,12 @@
+package com.iot.api.IoTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTapiApplication.class, args);
+	}
+}
